92260be68240bbe2a7b5b67d716fe7b2027116db
vaguilar25 <vivian.aguilar@gmail.com>
2019-01-26 20:11:35 -0600

    Updates

---
+++ precache-manifest.4b6b5ff2db3e9e6e16f7330eaa98fc68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3e0f9c71b1c2b5feaa72266f108b36",
+    "url": "/clicky-game/static/media/header.db3e0f9c.jpg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "71570e0ee8e8f9ccbc25",
+    "url": "/clicky-game/static/js/main.71570e0e.chunk.js"
+  },
+  {
+    "revision": "405fe43fb4b888bea6c1",
+    "url": "/clicky-game/static/js/1.405fe43f.chunk.js"
+  },
+  {
+    "revision": "71570e0ee8e8f9ccbc25",
+    "url": "/clicky-game/static/css/main.c48dc6cf.chunk.css"
+  },
+  {
+    "revision": "300b91b120ce8fecccc74e4d4a441890",
+    "url": "/clicky-game/index.html"
+  }
+];
